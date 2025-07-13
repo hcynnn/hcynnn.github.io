2881010a4460ceecea7ef033bc6be339afc25dcf
han <1279273848@qq.com>
2025-07-13 13:10:39 +0800

    Add files via upload

---
+++ swi/src/settings.68ef8.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/game.fire",orientation:"",jsList:[],bundleVers:{resources:"60c09",internal:"52d36",main:"a5132"}};
